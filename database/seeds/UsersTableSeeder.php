7c534982b5537a8a7513693bcff8d74958af1102
JxMarco <jxqn_liu@163.com>
2020-04-12 08:46:51 +0000

    优化用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $users = User::find(1);
+        $users->name = 'Marco';
+        $users->email = 'jxqn_liu@163.com';
+        $users->save();
+    }
+}
